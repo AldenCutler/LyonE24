cd951ff3957cf5bc4836503cdb98dabed0aa9b20
menmob <memobley@wpi.edu>
2024-07-03 20:29:14 +0200

    map stuff

--- max-experiments/access-grid-heatmap/make_map.py
+++ max-experiments/access-grid-heatmap/make_map.py
@@ -1,57 +1,189 @@
-import matplotlib.pyplot as plt
-import json
-import pandas as pd
+
+
+
+
+import sqlite3
 import geopandas as gpd
-from shapely.geometry import Point
-import numpy as np
+import json
+from datetime import datetime
+import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+from folium.plugins import HeatMap
 
-# Load the JSON data
-with open('./data/stops.json') as f:
-    data = json.load(f)[0]['rows']
+conn = sqlite3.connect('../itinerary-scraping/journeys.db')
+cursor = conn.cursor()
 
 
-# Extract coordinates and names
-stops = [
-    {"name": stop[1], "lon": float(json.loads(stop[2])['lon']), "lat": float(json.loads(stop[2])['lat'])}
-    for stop in data if len(json.loads(stop[3])) != 0
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
+
+
+
+
+
+cursor.execute('SELECT * FROM outages')
+outages = cursor.fetchall()
+
+cursor.execute('SELECT * FROM journeys')
+journeys = cursor.fetchall()
+
+cursor.execute('SELECT * FROM stops')
+stops = cursor.fetchall()
+
+cursor.close()
+conn.close()
+
+
+
+
+
+outages_df = pd.DataFrame(outages, columns=['outage_id', 'stop_id', 'effect', 'updated_at', 'outage_data'])
+stops_df = pd.DataFrame(stops, columns=['stop_id', 'stop_name', 'stop_coords', 'stop_acc'])
+
+
+merged_df = pd.merge(outages_df, stops_df, on='stop_id')
+
+
+
+
+
+tcl_metro = gpd.read_file('data/tcl_metro.json')
+tcl_metro = tcl_metro.drop(columns=['date_debut', 'date_fin', 'last_update', 'last_update_fme'])
+
+
+
+
+        
+tcl_metro['style'] = [
+    {
+        "color": "black",
+        "weight": 5,
+        "opacity": 1
+    }
+    for i in range(len(tcl_metro))
 ]
 
-stops = {stop['name']: stop for stop in stops}.values()
 
-# Create a DataFrame
-df = pd.DataFrame(stops)
 
-# Create a GeoDataFrame
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat))
 
-# Plot the stops
-fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-gdf.plot(ax=ax, color='black', alpha=1, markersize=30)
 
-# Customizing the map
-ax.set_axis_off()
-fig.patch.set_facecolor('#e4dec0')  # Setting background color to match the poster
 
-# Center of Lyon, France
-lyon_center = Point(4.8357, 45.7640)  # Longitude, Latitude
+merged_df['lon'] = merged_df['stop_coords'].apply(lambda x: x.split(',')[0][9:-1])
+merged_df['lat'] = merged_df['stop_coords'].apply(lambda x: x.split(',')[1][9:-2])
+merged_df = merged_df.drop(columns=['stop_coords'])
 
-# Radii in miles
-radii = [1, 3, 5, 7, 9]
 
-# Conversion from miles to degrees approximately (since 1 degree latitude ~ 69 miles)
-miles_to_degrees = 1 / 69.0
+merged_df['outage_data'] = merged_df['outage_data'].apply(lambda x: json.loads(x))
+merged_df['stop_acc'] = merged_df['stop_acc'].apply(lambda x: json.loads(x))
+begin, end = [], []
+cause, effect = [], []
+for row in merged_df['outage_data']:
+    
+    
+    
+    times = row['periods']    
+    begin.append(datetime.strptime(times[0]['begin'], '%Y%m%dT%H%M%S').isoformat() + '+02:00')
+    end.append(datetime.strptime(times[0]['end'], '%Y%m%dT%H%M%S').isoformat() + '+02:00')
+    
+    
+    cause.append(row['cause'])
+    effect.append(row['effect'])
+    
+merged_df['begin'] = begin
+merged_df['end'] = end
+merged_df['cause'] = cause
+merged_df['effect'] = effect
 
-for radius in radii:
-    circle = plt.Circle(
-        (lyon_center.x, lyon_center.y), 
-        radius * miles_to_degrees, 
-        color='grey', 
-        fill=True, 
-        linewidth=3,
-        alpha=0.5
-    )
-    #ax.add_patch(circle)
+merged_df = merged_df.drop(columns=['outage_data'])
 
-# Save the map
-output_path = './out/lyon_tcl_map_with_circles.png'
-plt.savefig(output_path, dpi=300, bbox_inches='tight', pad_inches=0.1)
+
+merged_df['effect'] = merged_df['effect'].apply(lambda x: x['label'])
+merged_df['cause'] = merged_df['cause'].apply(lambda x: x['label'])
+merged_df.head()
+
+
+
+m = folium.Map(location=[45.75, 4.85], zoom_start=13, tiles='cartodb voyager')
+
+
+heat_data = [[row['lat'], row['lon']] for _, row in merged_df.iterrows()]
+HeatMap(heat_data).add_to(m)
+
+
+marker_cluster = MarkerCluster().add_to(m)
+for idx, row in merged_df.iterrows():
+    tooltip = f"Stop Name: {row['stop_name']}<br>Outage ID: {row['outage_id']}<br>Start: {row['begin']}<br>End: {row['end']}<br>Cause: {row['cause']}<br>Effect: {row['effect']}"
+                
+    folium.Marker([row['lat'], row['lon']], tooltip=tooltip).add_to(marker_cluster)
+
+
+folium.GeoJson(tcl_metro, style_function=lambda x: x['properties']['style']).add_to(m)
+
+
+'''
+Outage database only contains data on metro line/stop outages, so bus lines are not displayed by default.
+The bus line data is still useful to see how metro lines are connected to bus lines and thus how outages on metro lines could affect bus lines.
+Uncomment the following lines to display the bus lines.
+'''
+
+
+
+
+m.save('./data/outages_heatmap.html')
+
+
+
+m = folium.Map(location=[45.75, 4.85], zoom_start=13, tiles='cartodb voyager')
+
+
+heat_data = [[row['lat'], row['lon']] for _, row in merged_df.iterrows()]
+HeatMap(heat_data).add_to(m)
+
+
+
+                
+
+    
+folium.GeoJson(tcl_metro, style_function=lambda x: x['properties']['style']).add_to(m)
+m.save('./data/outages_heatmap_no_cluster.html')
+
+
+
+merged_df = merged_df[merged_df['effect'] != '.']
+merged_df.head()
+
+
+
+m = folium.Map(location=[45.75, 4.85], zoom_start=13, tiles='None')
+
+
+heat_data = [[row['lat'], row['lon']] for _, row in merged_df.iterrows()]
+HeatMap(heat_data).add_to(m)
+
+
+marker_cluster = MarkerCluster().add_to(m)
+
+for idx, row in merged_df.iterrows():
+    tooltip = f"Stop Name: {row['stop_name']}<br>Outage ID: {row['outage_id']}<br>Start: {row['begin']}<br>End: {row['end']}<br>Cause: {row['cause']}<br>Effect: {row['effect']}"
+                
+    folium.Marker([row['lat'], row['lon']], tooltip=tooltip).add_to(marker_cluster)
+    
+
+folium.GeoJson(tcl_metro, style_function=lambda x: x['properties']['style']).add_to(m)
+
+'''
+Outage database only contains data on metro line/stop outages, so bus lines are not displayed by default.
+The bus line data is still useful to see how metro lines are connected to bus lines and thus how outages on metro lines could affect bus lines.
+Uncomment the following lines to display the bus lines.
+'''
+
+
+
+
+m.save('./out/filtered_effect_outages.html')
+
+
+
+
+
